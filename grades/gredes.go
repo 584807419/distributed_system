ca22f6a8c320cf9c210909eaf5c2824680f8e5fc
584807419 <584807419@qq.com>
2022-12-02 21:06:02 +0800

    business service one

---
+++ grades/gredes.go
@@ -0,0 +1,47 @@
+package grades
+
+import "fmt"
+
+type GradeType string
+
+type Grade struct {
+	Title string
+	Type  GradeType
+	Score float32
+}
+
+type Student struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Grades    []Grade
+}
+
+// 学生平均成绩计算
+func (s Student) Average() float32 {
+	var result float32
+	for _, grade := range s.Grades {
+		result += grade.Score
+	}
+	return result / float32(len(s.Grades))
+}
+
+const (
+	GradeQuiz = GradeType("Quiz")
+	GradeTest = GradeType("Test")
+	GradeExam = GradeType("Exam")
+)
+
+type Students []Student
+
+var students Students
+
+// 通过学生的id找到学生
+func (ss Students) GetByID(id int) (*Student, error) {
+	for i := range ss {
+		if ss[i].ID == id {
+			return &ss[i], nil
+		}
+	}
+	return nil, fmt.Errorf("Student with ID %d not found", id)
+}
---
+++ grades/mockdata.go
@@ -0,0 +1,83 @@
+package grades
+
+func init() {
+	students = []Student{
+		{
+			ID:        1,
+			FirstName: "Nick",
+			LastName:  "Carter",
+			Grades: []Grade{
+				{Title: "Quiz 1",
+					Type:  GradeQuiz,
+					Score: 85},
+				{Title: "final exam",
+					Type:  GradeExam,
+					Score: 94},
+				{Title: "quiz 2",
+					Type:  GradeQuiz,
+					Score: 82},
+			},
+		},
+		{
+			ID:        2,
+			FirstName: "roberto",
+			LastName:  "Baggio",
+			Grades: []Grade{
+				{Title: "Quiz 1",
+					Type:  GradeQuiz,
+					Score: 100},
+				{Title: "final exam",
+					Type:  GradeExam,
+					Score: 100},
+				{Title: "quiz 2",
+					Type:  GradeQuiz,
+					Score: 81},
+			},
+		}, {
+			ID:        3,
+			FirstName: "emma",
+			LastName:  "stone",
+			Grades: []Grade{
+				{Title: "Quiz 1",
+					Type:  GradeQuiz,
+					Score: 67},
+				{Title: "final exam",
+					Type:  GradeExam,
+					Score: 0},
+				{Title: "quiz 2",
+					Type:  GradeQuiz,
+					Score: 75},
+			},
+		}, {
+			ID:        4,
+			FirstName: "rachel",
+			LastName:  "mcadams",
+			Grades: []Grade{
+				{Title: "Quiz 1",
+					Type:  GradeQuiz,
+					Score: 98},
+				{Title: "final exam",
+					Type:  GradeExam,
+					Score: 99},
+				{Title: "quiz 2",
+					Type:  GradeQuiz,
+					Score: 94},
+			},
+		}, {
+			ID:        5,
+			FirstName: "kelly",
+			LastName:  "clarkson",
+			Grades: []Grade{
+				{Title: "Quiz 1",
+					Type:  GradeQuiz,
+					Score: 95},
+				{Title: "final exam",
+					Type:  GradeExam,
+					Score: 100},
+				{Title: "quiz 2",
+					Type:  GradeQuiz,
+					Score: 97},
+			},
+		},
+	}
+}
