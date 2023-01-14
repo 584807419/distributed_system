5224586ddbbaed6fa9d8543c270995f4b3947ddb
584807419 <584807419@qq.com>
2023-01-14 22:58:42 +0800

    add service discovery code

--- cmd/gradingservice/main.go
+++ cmd/gradingservice/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"distributed_system/grades"
+	"distributed_system/log"
 	"distributed_system/registry"
 	"distributed_system/service"
 	"fmt"
@@ -13,12 +14,19 @@ func main() {
 	host, port := "localhost", "6000"
 	serviceAddress := fmt.Sprintf("http://%s:%s", host, port)
 	r := registry.Registration{
-		ServiceName: registry.GradingService,
-		ServiceURL:  serviceAddress,
+		ServiceName:      registry.GradingService,
+		ServiceURL:       serviceAddress,
+		RequiredServices: []registry.ServiceName{registry.LogService},
+		ServiceUpdateURL: serviceAddress + "/services",
 	}
 	ctx, err := service.Start(context.Background(), host, port, r, grades.RegisterHandlers)
 	if err != nil {
 		stlog.Fatalln(err)
+	}
+
+	if logProvider, err := registry.GetProvider(registry.LogService); err == nil {
+		fmt.Printf("Logging service found at:%s\n", logProvider)
+		log.SetClientLogger(logProvider, r.ServiceName)
 	}
 	<-ctx.Done()
 	fmt.Println("shutting down grading service.")
--- registry/registration.go
+++ registry/registration.go
@@ -3,11 +3,23 @@ package registry
 type ServiceName string
 
 type Registration struct {
-	ServiceName ServiceName
-	ServiceURL  string
+	ServiceName      ServiceName
+	ServiceURL       string
+	RequiredServices []ServiceName // 当前服务依赖的其他服务
+	ServiceUpdateURL string
 }
 
 const (
-	LogService = ServiceName("LogService")
+	LogService     = ServiceName("LogService")
 	GradingService = ServiceName("GradingService")
 )
+
+type patchEntry struct {
+	Name ServiceName
+	URL  string
+}
+
+type patch struct {
+	Added   []patchEntry
+	Removed []patchEntry
+}
